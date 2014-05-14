0e621273a89382281165af0c5060a7e47f66a4db
devkont <yusupovb@gmail.com>
2014-05-14 09:50:29 +0400

    Корректировка setup-файла

--- simprojects/simprojects.setup.php
+++ simprojects/simprojects.setup.php
@@ -2,10 +2,10 @@
 
 /* ====================
  * [BEGIN_COT_EXT]
- * Code=simproducts
- * Name=Similar products
+ * Code=simprojects
+ * Name=Similar projects
  * Category=
- * Description=Похожие товары
+ * Description=Похожие проекты
  * Version=1.0.0
  * Date=
  * Author=CMSWorks Team
@@ -15,19 +15,19 @@
  * Lock_guests=12345A
  * Auth_members=RW
  * Lock_members=12345A
- * Requires_modules=market
+ * Requires_modules=projects
  * [END_COT_EXT]
  *
  * [BEGIN_COT_EXT_CONFIG]
  * relev=01:select:0,1,2,3,4,5,6,7:3:Релевантность
- * limit=02:select:0,1,2,3,4,5,10,15,20,25,30,35,40,45,50:5:Число похожих товаров для вывода
+ * limit=02:select:0,1,2,3,4,5,10,15,20,25,30,35,40,45,50:5:Число похожих проектов для вывода
  * [END_COT_EXT_CONFIG]
  */
 
 /**
- * simproducts plugin
+ * simprojects plugin
  *
- * @package simproducts
+ * @package simprojects
  * @version 1.0.0
  * @author CMSWorks Team
  * @copyright Copyright (c) CMSWorks.ru
